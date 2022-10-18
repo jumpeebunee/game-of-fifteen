87e58790b9bc8d19d613c824b0ad10c2b673b5a9
Ben <jumpeebunee@gmail.com>
2022-10-18 13:15:22 +0300

    feat: add moving cell

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,35 @@ function init() {
 
     const config = {
         size: 4,
+    };  
+
+    const move = {
+        checkPosition(e) {
+            const currentCell = e.target.id;
+
+            const currentI = +currentCell.split('-')[0];
+            const currentJ = +currentCell.split('-')[1];
+
+            const checkLeft = document.getElementById(`${currentI}-${currentJ - 1}`);
+            const checkRight = document.getElementById(`${currentI}-${currentJ + 1}`);
+            const checkTop = document.getElementById(`${currentI - 1}-${currentJ}`);
+            const checkBottom = document.getElementById(`${currentI + 1}-${currentJ}`);
+
+            if (checkLeft && checkLeft.dataset.isEmpty)  {
+                move.nextPosition(checkLeft, e.target);
+            } else if (checkRight && checkRight.dataset.isEmpty) {
+                move.nextPosition(checkRight, e.target);
+            } else if (checkTop && checkTop.dataset.isEmpty) {
+                move.nextPosition(checkTop, e.target);
+            } else if (checkBottom && checkBottom.dataset.isEmpty) {
+                move.nextPosition(checkBottom, e.target);
+            }
+        },
+        nextPosition(to, from) {
+            [to.id, from.id] = [from.id, to.id];
+            [to.style.left, from.style.left] = [from.style.left, to.style.left];
+            [to.style.top, from.style.top] = [from.style.top, to.style.top];
+        },
     };
 
     const board =  {
@@ -33,9 +62,22 @@ function init() {
                 for (let j = 0; j < config.size; j++) {
                     const cell = document.createElement('div');
                     cell.classList.add('cell');
+
+                    const cellNumber = cellsArray.shift();
+
+                    if (cellNumber === null) {
+                        cell.classList.add('cell-empty');
+                        cell.dataset.isEmpty = 'empty';
+                    } else {
+                        cell.textContent = cellNumber;
+                    };
+
+                    cell.id = `${i}-${j}`;
+
                     cell.style.left = `${22 + (118 * j) + (10 * j)}px`;
                     cell.style.top = `${22 + (118 * i) + (10 * i)}px`;
-                    cell.textContent = cellsArray.shift();
+                    cell.addEventListener('click', move.checkPosition);
+
                     board.append(cell);
                 };
             };
